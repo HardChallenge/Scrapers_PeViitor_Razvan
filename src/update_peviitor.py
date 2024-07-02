acb9cc741ddf4fa761f7ea6415179b253465c68b
Chichirau Razvan <chichiraurazvan@yahoo.com>
2024-07-02 21:46:16 +0300

    Added user-agent header on company and logo

--- src/update_peviitor.py
+++ src/update_peviitor.py
@@ -8,23 +8,23 @@ class UpdatePeViitor:
     POST_URL = "https://api.peviitor.ro/v5/add/"
     LOGO_URL = "https://api.peviitor.ro/v1/logo/add/"
     TOKEN_URL = "https://api.peviitor.ro/v5/get_token/"
-    USER_AGENT = "'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36'"
+    USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36"
 
     def __init__(self):
         self.POST_HEADER = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.get_token()}",
+            "User-Agent": self.USER_AGENT,
         }
 
         self.LOGO_HEADER = {
             "Content-Type": "application/json",
+            "User-Agent": self.USER_AGENT,
         }
 
     def get_token(self):
-        token_endpoint = "https://api.peviitor.ro/v5/get_token/"
-
         token = requests.post(
-            token_endpoint,
+            self.TOKEN_URL,
             json={"email": "chichiraurazvan@yahoo.com"},
             headers={"User-Agent": self.USER_AGENT},
         )
@@ -42,7 +42,6 @@ class UpdatePeViitor:
 
         if post_request_to_server.status_code != 200:
             print(f"Error updating {company_name} jobs")
-            print(post_request_to_server.text)
 
     def update_logo(self, id_company: str, logo_link: str):
 
@@ -51,4 +50,3 @@ class UpdatePeViitor:
 
         if response.status_code != 200:
             print(f"Error updating {id_company} logo")
-            print(response.text)
