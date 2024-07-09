d4fcdbcb373a5efce7839759248326600b6f95ce
Chichirau Razvan <chichiraurazvan@yahoo.com>
2024-07-09 20:04:32 +0300

    Changed LOGO_URL endpoint

--- src/update_peviitor.py
+++ src/update_peviitor.py
@@ -6,7 +6,7 @@ import time
 
 class UpdatePeViitor:
     POST_URL = "https://api.peviitor.ro/v5/add/"
-    LOGO_URL = "https://api.peviitor.ro/v1/logo/add/"
+    LOGO_URL = "https://api.peviitor.ro/v3/logo/add/"
     TOKEN_URL = "https://api.peviitor.ro/v5/get_token/"
     USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36"
 
